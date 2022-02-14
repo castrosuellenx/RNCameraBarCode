77dbcb4206ee28c633fed6499f8da38a61bfca69
Suellen Castro <castrosuellenx@gmail.com>
2022-02-14 16:17:35 -0300

    update screen barcodeReader

--- src/screens/BarCodeReader/index.tsx
+++ src/screens/BarCodeReader/index.tsx
@@ -1,20 +1,13 @@
-import Guidelines from '@/components/Guidelines';
-import { useNavigation } from '@react-navigation/native';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useTheme } from 'styled-components/native';
+
+import Camera from '@/components/Camera';
+import Guidelines from '@/components/Guidelines';
 
 import * as S from './styles';
 
 const BarCodeReader: React.FC = () => {
   const theme = useTheme();
-  const { navigate } = useNavigation();
-
-  // Função simulando leitura do código de barras // TEMPORARIO
-  useEffect(() => {
-    setTimeout(() => {
-      navigate('Result');
-    }, 2000);
-  }, [navigate]);
 
   return (
     <S.Container>
@@ -29,7 +22,9 @@ const BarCodeReader: React.FC = () => {
       </S.Title>
 
       <Guidelines>
-        <S.ContentCamera />
+        <S.ContentCamera>
+          <Camera />
+        </S.ContentCamera>
       </Guidelines>
 
       <S.Subtitle>Barcode</S.Subtitle>
