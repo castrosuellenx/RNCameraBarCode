f68e75bcdee73f3e50c2cdb682e3e693aefb6e56
Suellen Castro <castrosuellenx@gmail.com>
2022-02-14 12:33:59 -0300

    add styles on BarCodeReader screen

--- src/screens/BarCodeReader/index.tsx
+++ src/screens/BarCodeReader/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import Guidelines from '@/components/Guidelines';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { useTheme } from 'styled-components/native';
 
 import * as S from './styles';
 
 const BarCodeReader: React.FC = () => {
+  const theme = useTheme();
+  const { navigate } = useNavigation();
+
+  // Função simulando leitura do código de barras // TEMPORARIO
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('Result');
+    }, 2000);
+  }, [navigate]);
+
   return (
     <S.Container>
-      <S.Title>BarCodeReader</S.Title>
+      <S.StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.secondaryMain}
+      />
+
+      <S.Title>
+        Aponte a câmera para o{'\n'}
+        código de barras e aguarde.
+      </S.Title>
+
+      <Guidelines>
+        <S.ContentCamera />
+      </Guidelines>
+
+      <S.Subtitle>Código de Barras</S.Subtitle>
     </S.Container>
   );
 };
--- src/screens/BarCodeReader/styles.ts
+++ src/screens/BarCodeReader/styles.ts
@@ -1,7 +1,35 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.secondaryMain};
 `;
 
-export const Title = styled.Text``;
+export const StatusBar = styled.StatusBar``;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${RFValue(17)}px;
+  color: ${({ theme }) => theme.colors.white};
+  text-align: center;
+  margin-bottom: ${RFValue(30)}px;
+`;
+
+export const ContentCamera = styled.View`
+  height: ${RFValue(250)}px;
+  width: ${RFValue(250)}px;
+  align-self: center;
+  border-radius: ${RFValue(5)}px;
+  background-color: ${({ theme }) => theme.colors.secondaryMedium};
+  position: absolute;
+`;
+
+export const Subtitle = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${RFValue(13)}px;
+  color: ${({ theme }) => theme.colors.grey};
+  margin-top: ${RFValue(7)}px;
+`;
