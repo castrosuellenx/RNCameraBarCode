ce26e4ffdebc9436583d4bb1fd9498314619210f
Suellen Castro <castrosuellenx@gmail.com>
2022-02-14 14:32:32 -0300

    update screen Result with styles

--- src/screens/Result/index.tsx
+++ src/screens/Result/index.tsx
@@ -1,11 +1,36 @@
+import ButtonPrimary from '@/components/ButtonPrimary';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { useTheme } from 'styled-components/native';
 
 import * as S from './styles';
 
 const Result: React.FC = () => {
+  const theme = useTheme();
+  const { navigate } = useNavigation();
+
+  const onTryAgain = () => {
+    navigate('BarCodeReader');
+  };
+
   return (
     <S.Container>
+      <S.StatusBar
+        barStyle={theme.colors.barStyle}
+        backgroundColor={theme.colors.background}
+      />
+
       <S.Title>Result</S.Title>
+
+      <S.Content>
+        <S.TitleResult>Your barcode</S.TitleResult>
+        <S.SubtitleResult>2447892235489</S.SubtitleResult>
+
+        <S.TitleResult>Your barcode type</S.TitleResult>
+        <S.SubtitleResultType>2447892235489</S.SubtitleResultType>
+      </S.Content>
+
+      <ButtonPrimary title="Try again" onPress={onTryAgain} />
     </S.Container>
   );
 };
--- src/screens/Result/styles.ts
+++ src/screens/Result/styles.ts
@@ -1,7 +1,50 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text``;
+export const StatusBar = styled.StatusBar``;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(50)}px;
+  color: ${({ theme }) => theme.colors.primaryMain};
+  text-align: center;
+  margin-bottom: ${RFValue(5)}px;
+`;
+
+export const Content = styled.View`
+  width: ${RFValue(300)}px;
+  align-items: center;
+  justify-content: center;
+  margin-top: ${RFValue(20)}px;
+  margin-bottom: ${RFValue(20)}px;
+  border-radius: ${RFValue(10)}px;
+  padding-vertical: ${RFValue(40)}px;
+  background-color: ${({ theme }) => theme.colors.primaryLight};
+`;
+
+export const TitleResult = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  font-size: ${RFValue(22)}px;
+  color: ${({ theme }) => theme.colors.secondaryMain};
+  margin-bottom: ${RFValue(5)}px;
+`;
+
+export const SubtitleResult = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.secondaryMedium};
+  margin-bottom: ${RFValue(30)}px;
+`;
+
+export const SubtitleResultType = styled.Text`
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.secondaryMedium};
+`;
